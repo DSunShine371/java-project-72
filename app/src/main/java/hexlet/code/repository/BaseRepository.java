62582844fa06a28684644f60bd5db2e6c7bb86af
DSunShine371 <pestic077@gmail.com>
2025-09-08 13:32:45 +0300

    Step 8.5 refactor BaseRepository (old env) and update schema.sql

--- app/src/main/java/hexlet/code/repository/BaseRepository.java
+++ app/src/main/java/hexlet/code/repository/BaseRepository.java
@@ -3,15 +3,19 @@ package hexlet.code.repository;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import java.util.Objects;
-
 public class BaseRepository {
     protected static final HikariDataSource DATA_SOURCE;
 
     static {
         HikariConfig config = new HikariConfig();
         String jdbcUrl = System.getenv("DATABASE_URL");
-        config.setJdbcUrl(Objects.requireNonNullElse(jdbcUrl, "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;"));
+        if (jdbcUrl != null && !jdbcUrl.isEmpty()) {
+            config.setJdbcUrl(jdbcUrl);
+            config.setDriverClassName("org.postgresql.Driver");
+        } else {
+            config.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+            config.setDriverClassName("org.h2.Driver");
+        }
         DATA_SOURCE = new HikariDataSource(config);
     }
 
